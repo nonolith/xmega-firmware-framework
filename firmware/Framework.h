e058086c23d880572d4e245b0adf0d1b2c51bb22
Ian Daniher <it.daniher@gmail.com>
2012-08-08 15:26:03 -0400

    inital commit of basic xmega firmware framework

---
+++ firmware/Framework.h
@@ -0,0 +1,8 @@
+
+#define F_CPU 32000000UL
+
+#include "usb/avr/io.h"
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include "Descriptors.h"
+#include "usb/usb.h"
---
+++ firmware/main.c
@@ -0,0 +1,70 @@
+#include "Framework.h"
+
+int main(void){
+	USB_ConfigureClock();
+	PORTR.DIRSET = 1 << 1;
+	PORTR.OUTSET = 1 << 1;
+	USB_Init();
+	sei();	
+
+	for (;;){
+		USB_Evt_Task();
+		USB_Task();
+	}
+
+}
+
+#define xstringify(s) stringify(s)
+#define stringify(s) #s
+
+const char PROGMEM hwversion[] = xstringify(HW_VERSION);
+const char PROGMEM fwversion[] = xstringify(FW_VERSION);
+
+uint8_t usb_cmd = 0;
+uint8_t cmd_data = 0;
+
+/** Event handler for the library USB Control Request reception event. */
+bool EVENT_USB_Device_ControlRequest(USB_Request_Header_t* req){
+	// zero out ep0_buf_in
+	for (uint8_t i = 0; i < 64; i++) ep0_buf_in[i] = 0;
+	usb_cmd = 0;
+	if ((req->bmRequestType & CONTROL_REQTYPE_TYPE) == REQTYPE_VENDOR){
+		switch(req->bRequest){
+			case 0x00: // Info
+				if (req->wIndex == 0){
+					USB_ep0_send_progmem((uint8_t*)hwversion, sizeof(hwversion));
+				}else if (req->wIndex == 1){
+					USB_ep0_send_progmem((uint8_t*)fwversion, sizeof(fwversion));
+				}
+				
+				return true;
+
+			// read EEPROM	
+			case 0xE0: 
+				eeprom_read_block(ep0_buf_in, (void*)(req->wIndex*64), 64);
+				USB_ep0_send(64);
+				return true;
+
+			// write EEPROM	
+			case 0xE1: 
+				usb_cmd = req->bRequest;
+				cmd_data = req->wIndex;
+				USB_ep0_send(0);
+				return true; // Wait for OUT data (expecting an OUT transfer)
+
+			// disconnect from USB, jump to bootloader	
+			case 0xBB: 
+				USB_enter_bootloader();
+				return true;
+		}
+	}
+	return false;
+}
+
+void EVENT_USB_Device_ControlOUT(uint8_t* buf, uint8_t count){
+	switch (usb_cmd){
+		case 0xE1: // Write EEPROM
+			eeprom_update_block(buf, (void*)(cmd_data*64), count);
+			break;
+	}
+}
